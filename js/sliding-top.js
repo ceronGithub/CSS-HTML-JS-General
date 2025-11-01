4f941d439fdb8438a14b8757eb2a9566aedd10d5
cerongithub <developerceron@gmail.com>
2025-11-01 10:45:21 +0800

    first commit

---
+++ js/sliding-top.js
@@ -0,0 +1,8 @@
+const _open_button = document.querySelector("#fa-sliders-id");
+const _sliding_card = document.querySelector(".sliding-card");
+ _open_button.addEventListener("click", show_navbar);
+
+ function show_navbar(){   
+    _open_button.classList.toggle('fa-active'); 
+    _sliding_card.classList.toggle('expand-sliding-card');
+}
