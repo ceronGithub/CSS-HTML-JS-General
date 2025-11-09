0c13174cabf4627cd35799947146e06137c45817
cerongithub <developerceron@gmail.com>
2025-11-09 17:24:10 +0800

    pop-navbar-top done

--- js/general-navbar-folder/sliding-left.js
+++ js/general-navbar-folder/sliding-left.js
@@ -2,7 +2,7 @@ const _open_button = document.querySelector("#fa-bars-id");
 const _sliding_card = document.querySelector(".sliding-card");
  _open_button.addEventListener("click", show_navbar);
 
- function show_navbar(){       
+function show_navbar(){       
     _open_button.classList.toggle('fa-active');
     _sliding_card.classList.toggle('expand-sliding-card');
 }
